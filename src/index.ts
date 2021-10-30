d571ffa75c9a1f6781bae36457e71f940ddc22fa
Ray Park <reikop.com@gmail.com>
2021-10-31 00:19:43 +0900

    IMPORT ERROR 수정

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export * from "./types/types";
-// const CalculationEquipPvPStatus = require('./utils/CalcPvP');
-// export default CalculationEquipPvPStatus;
-exports.CalculationEquipPvPStatus = require('./utils/CalcPvP')
+export * from "./utils/CalcPvP";
